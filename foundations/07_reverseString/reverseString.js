bfd652bea7413cffba75753008c04691d13d3d37
Jules Nathan <187531836+Kirschtaria06@users.noreply.github.com>
2026-01-04 23:43:06 +0800

    Completed 07_reverseString

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let charsArray = string.split("")
+    let reversedArray = charsArray.reverse()
+    let joined = reversedArray.join("")
+    return joined
 };
 
 // Do not edit below this line
